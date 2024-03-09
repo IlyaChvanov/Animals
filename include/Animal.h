b07b59246aa851f2840b17f52a8052abfc434ab2
Ilya Chvanov <ivchvanov@edu.hse.ru>
2024-03-10 00:01:21 +0300

    warnings

--- include/Animal.h
+++ include/Animal.h
@@ -3,98 +3,113 @@
 //
 #include "iostream"
 #include "string"
+
 #ifndef ANIMALS_ANIMALS_H
 #define ANIMALS_ANIMALS_H
 using namespace std;
 
-class Animal
-{
-public:
+class Animal {
+ public:
     Animal();
-    Animal(float mass, char* sex_new, string&& color, int age);
-    Animal(const Animal& other);
-    Animal(Animal&& other) noexcept;
+    Animal(float mass, char *sex_new, string &&color, int age);
+    Animal(const Animal &other);
+    Animal(Animal &&other) noexcept;
     virtual ~Animal();
 
-    Animal& operator=(const Animal& other);
-    Animal& operator=(Animal&& moved) noexcept;
+    Animal &operator=(const Animal &other);
+    Animal &operator=(Animal &&moved) noexcept;
 
-    float GetMass()const;
-    char* GetSex()const;
-    int GetAge()const;
+    float GetMass() const;
+    char *GetSex() const;
+    int GetAge() const;
+
     string GetColor();
 
     void SetMass(float mass);
-    void SetSex(char* new_sex);
+    void SetSex(char *new_sex);
     void SetAge(int new_age);
-    void SetColor(string&& new_color);
+    void SetColor(string &&new_color);
 
-    virtual string WhatDoesSay(){return "";};
-protected:
+    virtual string WhatDoesSay() { return ""; }
+ protected:
     float mass{0};
-    char* sex{nullptr};
+    char *sex{nullptr};
     string color;
     int age{0};
 };
 
-class Dog: public Animal
-{
-public:
-    enum class Breeds{German_Shepherd, Golden_Retriever, Labrador_Retriever, Beagle, Yorkshire_Terrier, Poodle, French_Bulldog, Rottweiler, Dachshund, Pug};
+class Dog : public Animal {
+ public:
+    enum class Breeds {
+        German_Shepherd,
+        Golden_Retriever,
+        Labrador_Retriever,
+        Beagle,
+        Yorkshire_Terrier,
+        Poodle,
+        French_Bulldog,
+        Rottweiler,
+        Dachshund,
+        Pug
+    };
 
     Dog();
-    Dog(char* name, Breeds breed, float mass, char* sex, string&& color, int age);
-    Dog(const Dog& other);
-    Dog(Dog&& other)noexcept;
+
+    Dog(char *name, Breeds breed, float mass, char *sex, string &&color, int age);
+
+    Dog(const Dog &other);
+    Dog(Dog &&other) noexcept;
     ~Dog() override;
-
-    Dog& operator=(const Dog& other);
-    Dog& operator=(Dog&& moved) noexcept;
-
-    char* GetName();
+    Dog &operator=(const Dog &other);
+    Dog &operator=(Dog &&moved) noexcept;
+    char *GetName();
     int GetRace();
 
-
-    void SetName(char* new_name);
+    void SetName(char *new_name);
     void SetRace(Breeds new_breed);
+
     string WhatDoesSay() override;
 
-private:
+ private:
     Breeds breed = Breeds::German_Shepherd;
-    char* name = nullptr;
+    char *name = nullptr;
 
 };
 
-class Fox: public Animal
-{
-public:
-    enum class TypeFox{Red_Fox, Arctic_Fox, Fennec_Fox, Gray_Fox, Kit_Fox};
+class Fox : public Animal {
+ public:
+    enum class TypeFox {
+        Red_Fox, Arctic_Fox, Fennec_Fox, Gray_Fox, Kit_Fox
+    };
 
     Fox();
-    Fox(char* name, TypeFox type, float mass, char* sex, string&& color, int age);
-    Fox(const Fox& other);
-    Fox(Fox&& other)noexcept;
+    Fox(char *name, TypeFox type, float mass, char *sex, string &&color, int age);
+    Fox(const Fox &other);
+    Fox(Fox &&other) noexcept;
     ~Fox() override;
 
-    Fox& operator=(const Fox& other);
-    Fox& operator=(Fox&& moved) noexcept;
+    Fox &operator=(const Fox &other);
+    Fox &operator=(Fox &&moved) noexcept;
 
-    char* GetName();
+    char *GetName();
     int GetRace();
 
-
-    void SetName(char* new_name);
+    void SetName(char *new_name);
     void SetRace(TypeFox new_type);
-    string WhatDoesSay() override;
 
-    int GetNumberRabbitsEaten()const;
+    string WhatDoesSay() override;
+    int GetNumberRabbitsEaten() const;
+
     void SetNumberRabbitsEaten(int numberRabbitsEaten);
     void SetType(TypeFox new_type);
 
     void Eat();
-private:
-    char* name = nullptr;
+
+ private:
+    char *name = nullptr;
     TypeFox type = TypeFox::Red_Fox;
     int numberRabbitsEaten = 0;
 };
-#endif //ANIMALS_ANIMALS_H
+
+
+#endif
--- src/Animal.cpp
+++ src/Animal.cpp
@@ -4,10 +4,12 @@
 #include "string"
 #include "cstring"
 #include "../include/Animal.h"
+
 using namespace std;
-Animal:: Animal()=default;
-Animal::Animal(float mass, char *sex_new, string&& color, int age)
-{
+
+Animal::Animal() = default;
+
+Animal::Animal(float mass, char *sex_new, string &&color, int age) {
     this->mass = mass;
     size_t size_of_text = strlen(sex_new) + 1;
     sex = new char[size_of_text];
@@ -15,31 +17,40 @@ Animal::Animal(float mass, char *sex_new, string&& color, int age)
     this->color = color;
     this->age = age;
 }
-Animal::Animal(const Animal&)=default;
-Animal::Animal(Animal&& other) noexcept =default;
 
-Animal:: ~Animal() {delete[] sex;}
+Animal::Animal(const Animal &) = default;
 
-Animal& Animal:: operator=(const Animal& other)=default;
-Animal& Animal:: operator=(Animal&& moved) noexcept =default;
+Animal::Animal(Animal &&other) noexcept = default;
 
-string Animal::GetColor() {return color;}
-char* Animal::GetSex()const {return sex;}
-int Animal:: GetAge() const {return age;}
-float Animal:: GetMass() const {return mass;}
-void Animal::SetMass(float new_mass) {mass = new_mass;}
-void Animal::SetSex(char* new_sex)
-{
+Animal::~Animal() { delete[] sex; }
+
+Animal &Animal::operator=(const Animal &other) = default;
+
+Animal &Animal::operator=(Animal &&moved) noexcept = default;
+
+string Animal::GetColor() { return color; }
+
+char *Animal::GetSex() const { return sex; }
+
+int Animal::GetAge() const { return age; }
+
+float Animal::GetMass() const { return mass; }
+
+void Animal::SetMass(float new_mass) { mass = new_mass; }
+
+void Animal::SetSex(char *new_sex) {
     size_t size_of_text = strlen(new_sex) + 1;
     sex = new char[size_of_text];
     strcpy_s(this->sex, size_of_text, new_sex);
-};
-void Animal::SetAge(int new_age) {age = new_age;};
-void Animal::SetColor(string&& new_color) {color = new_color;};
+}
 
-Dog:: Dog()=default;
-Dog:: Dog(char* new_name, Breeds breed, float mass, char* new_sex, string&& color, int age)
-{
+void Animal::SetAge(int new_age) { age = new_age; }
+
+void Animal::SetColor(string &&new_color) { color = new_color; }
+
+Dog::Dog() = default;
+
+Dog::Dog(char *new_name, Breeds breed, float mass, char *new_sex, string &&color, int age) {
     size_t size_of_text_s = strlen(new_sex) + 1;
     sex = new char[size_of_text_s];
     strcpy_s(this->sex, size_of_text_s, new_sex);
@@ -53,29 +64,38 @@ Dog:: Dog(char* new_name, Breeds breed, float mass, char* new_sex, string&& colo
     this->color = color;
     this->age = age;
 }
-Dog:: ~Dog(){delete[] name; delete[] sex;}
 
-Dog::Dog(const Dog&)=default;
-Dog::Dog(Dog&& other) noexcept =default;
+Dog::~Dog() {
+    delete[] name;
+    delete[] sex;
+}
 
-Dog& Dog:: operator=(const Dog& other)=default;
-Dog& Dog:: operator=(Dog&& moved) noexcept =default;
+Dog::Dog(const Dog &) = default;
 
-void Dog::SetRace(Breeds new_breed) {breed = new_breed;}
-void Dog::SetName(char *new_name)
-{
+Dog::Dog(Dog &&other) noexcept = default;
+
+Dog &Dog::operator=(const Dog &other) = default;
+
+Dog &Dog::operator=(Dog &&moved) noexcept = default;
+
+void Dog::SetRace(Breeds new_breed) { breed = new_breed; }
+
+void Dog::SetName(char *new_name) {
     size_t size_of_text = strlen(new_name) + 1;
     name = new char[size_of_text];
     strcpy_s(this->name, size_of_text, new_name);
 }
-int Dog::GetRace() {return static_cast<int>(breed);}
-char* Dog::GetName() {return name;}
-string Dog::WhatDoesSay() {return "AF";}
+
+int Dog::GetRace() { return static_cast<int>(breed); }
+
+char *Dog::GetName() { return name; }
+
+string Dog::WhatDoesSay() { return "AF"; }
 
 
-Fox:: Fox()=default;
-Fox:: Fox(char* new_name, TypeFox type, float mass, char* new_sex, string&& color, int age)
-{
+Fox::Fox() = default;
+
+Fox::Fox(char *new_name, TypeFox type, float mass, char *new_sex, string &&color, int age) {
     size_t size_of_text_s = strlen(new_sex) + 1;
     sex = new char[size_of_text_s];
     strcpy_s(this->sex, size_of_text_s, new_sex);
@@ -89,29 +109,42 @@ Fox:: Fox(char* new_name, TypeFox type, float mass, char* new_sex, string&& colo
     this->color = color;
     this->age = age;
 }
-Fox:: ~Fox(){delete[] name; delete[] sex;}
 
-Fox::Fox(const Fox&)=default;
-Fox::Fox(Fox&& other) noexcept =default;
+Fox::~Fox() {
+    delete[] name;
+    delete[] sex;
+}
 
-Fox& Fox:: operator=(const Fox& other)=default;
-Fox& Fox:: operator=(Fox&& moved) noexcept =default;
+Fox::Fox(const Fox &) = default;
 
-void Fox::SetRace(TypeFox new_type) {type = new_type;}
-void Fox::SetName(char *new_name)
-{
+Fox::Fox(Fox &&other) noexcept = default;
+
+Fox &Fox::operator=(const Fox &other) = default;
+
+Fox &Fox::operator=(Fox &&moved) noexcept = default;
+
+void Fox::SetRace(TypeFox new_type) { type = new_type; }
+
+void Fox::SetName(char *new_name) {
     size_t size_of_text = strlen(new_name) + 1;
     name = new char[size_of_text];
     strcpy_s(this->name, size_of_text, new_name);
 }
-int Fox::GetRace() {return static_cast<int>(type);}
-char* Fox::GetName() {return name;}
-string Fox::WhatDoesSay() {return "Ring-ding-ding-ding-dingeringeding!\n"
-                                  "Gering-ding-ding-ding-dingeringeding!\n"
-                                  "Gering-ding-ding-ding-dingeringeding!";}
 
-int Fox::GetNumberRabbitsEaten()const{return numberRabbitsEaten;}
-void Fox::SetNumberRabbitsEaten(int new_numberRabbitsEaten) {numberRabbitsEaten = new_numberRabbitsEaten;}
-void Fox::SetType(TypeFox new_type){type = new_type;}
+int Fox::GetRace() { return static_cast<int>(type); }
 
-void Fox::Eat(){numberRabbitsEaten++;};
+char *Fox::GetName() { return name; }
+
+string Fox::WhatDoesSay() {
+    return "Ring-ding-ding-ding-dingeringeding!\n"
+           "Gering-ding-ding-ding-dingeringeding!\n"
+           "Gering-ding-ding-ding-dingeringeding!";
+}
+
+int Fox::GetNumberRabbitsEaten() const { return numberRabbitsEaten; }
+
+void Fox::SetNumberRabbitsEaten(int new_numberRabbitsEaten) { numberRabbitsEaten = new_numberRabbitsEaten; }
+
+void Fox::SetType(TypeFox new_type) { type = new_type; }
+
+void Fox::Eat() { numberRabbitsEaten++; }
